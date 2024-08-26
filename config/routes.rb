f6db0c13981632640b93b92ad876ad7751117a1e
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-08-26 11:21:07 +0000

    [start]Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
+   resources :books, only: [:new, :create, :index, :show, :destroy]
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
 end
